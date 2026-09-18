9c306224c4223e6f5bb6bfcdff7566e0e5f20a02
mimitrix123 <317410895+mimitrix123@users.noreply.github.com>
2026-09-18 05:56:56 +0530

    Add Week 4 Skybound Quest major project

---
+++ week-4/skybound-quest/android/app/src/main/java/com/example/skyboundquest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.skyboundquest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
+        Python.getInstance().getModule("main").callAttr("main")
+    }
+}
